c61326f63cb32c49ee21e90c59e88bc20b65764c
Josh <jvcoding9407@gmail.com>
2019-04-09 17:24:06 -0400

    style nav bar

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,59 +1,59 @@
-//toggle menu functionality
+// //toggle menu functionality
 
-//variables
-const toggleButton = document.querySelector(".toggle-menu__icon"); //button 
-const toggleButton_top = document.querySelector(".toggle-menu__icon--top");//top part
-const toggleButton_middle = document.querySelector(".toggle-menu__icon--middle");//middle part   
-const toggleButton_bottom = document.querySelector(".toggle-menu__icon--bottom");//bottom part
-let btnState = false; //boolean that keeps track of button state   
+// //variables
+// const toggleButton = document.querySelector(".toggle-menu__icon"); //button 
+// const toggleButton_top = document.querySelector(".toggle-menu__icon--top");//top part
+// const toggleButton_middle = document.querySelector(".toggle-menu__icon--middle");//middle part   
+// const toggleButton_bottom = document.querySelector(".toggle-menu__icon--bottom");//bottom part
+// let btnState = false; //boolean that keeps track of button state   
 
-//function
-const toggleOn = () => {
-    //top bar
-    toggleButton_top.style.transform = "rotate(45deg)";
-    toggleButton_top.style.transition = "transform .5s";
+// //function
+// const toggleOn = () => {
+//     //top bar
+//     toggleButton_top.style.transform = "rotate(45deg)";
+//     toggleButton_top.style.transition = "transform .5s";
 
-    //middle bar
-    toggleButton_middle.style.visibility = "hidden";
-    toggleButton_middle.style.opacity = "0";
-    toggleButton_middle.style.transition = "opacity .5s, visibility .5s";
+//     //middle bar
+//     toggleButton_middle.style.visibility = "hidden";
+//     toggleButton_middle.style.opacity = "0";
+//     toggleButton_middle.style.transition = "opacity .5s, visibility .5s";
 
-    //bottom bar
-    toggleButton_bottom.style.top = "0";
-    toggleButton_bottom.style.transform = "rotate(135deg)";
-    toggleButton_bottom.style.transition = "top .5s, transform .5s";
-};
+//     //bottom bar
+//     toggleButton_bottom.style.top = "0";
+//     toggleButton_bottom.style.transform = "rotate(135deg)";
+//     toggleButton_bottom.style.transition = "top .5s, transform .5s";
+// };
 
-const toggleOff = () => {
-    //top bar
-    toggleButton_top.style.transform = "rotate(180deg)";
-    toggleButton_top.style.transition = "transform .5s";
+// const toggleOff = () => {
+//     //top bar
+//     toggleButton_top.style.transform = "rotate(180deg)";
+//     toggleButton_top.style.transition = "transform .5s";
 
-    //middle bar
-    toggleButton_middle.style.visibility = "visible";
-    toggleButton_middle.style.opacity = "1";
-    toggleButton_middle.style.transition = "opacity .5s, visibility .5s";
+//     //middle bar
+//     toggleButton_middle.style.visibility = "visible";
+//     toggleButton_middle.style.opacity = "1";
+//     toggleButton_middle.style.transition = "opacity .5s, visibility .5s";
 
-    //bottom bar
-    toggleButton_bottom.style.top = "1rem";
-    toggleButton_bottom.style.transform = "rotate(-180deg)";
-    toggleButton_bottom.style.transition = "top .5s, transform .5s";
-}
+//     //bottom bar
+//     toggleButton_bottom.style.top = "1rem";
+//     toggleButton_bottom.style.transform = "rotate(-180deg)";
+//     toggleButton_bottom.style.transition = "top .5s, transform .5s";
+// }
 
-toggleButton.addEventListener("click", () => {
-    if (btnState) {
-        toggleOff();
-        btnState = false;
-    } else {
-        toggleOn();
-        btnState = true;
-    }
-});
+// toggleButton.addEventListener("click", () => {
+//     if (btnState) {
+//         toggleOff();
+//         btnState = false;
+//     } else {
+//         toggleOn();
+//         btnState = true;
+//     }
+// });
 
 //scroll functionality
 
 //variables
-const aboutMeSection = document.getElementById("about-me"); //about me section header
+const aboutMeSection = document.getElementById("scrollStop"); //about me section header
 const scrollDownArrow = document.querySelector(".hero__btn"); //scroll down button
 
 const header = document.querySelector(".header"); //header at top of page
@@ -68,3 +68,22 @@ scrollDownArrow.addEventListener("click", () => {  //scroll down function
 scrollUpArrow.addEventListener("click", () => {
     header.scrollIntoView({behavior:"smooth"});
 });
+
+// DROP DOWN MENU FUNCTIONALITY
+
+//VARIABLES
+const navBar = document.getElementById("navbar");
+
+//function
+const openNav = () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        navBar.style.top = "0";
+    } else {
+        navBar.style.top = "-60px";
+    }
+}
+
+//call function
+window.onscroll = () => {
+    openNav();
+}
