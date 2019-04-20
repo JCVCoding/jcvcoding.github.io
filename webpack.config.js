38afb68a8fd0016d17271be6f05102809fca07af
Josh <jvcoding9407@gmail.com>
2019-04-20 19:50:15 -0400

    minify JS

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+
+module.exports = {
+    entry: {
+        app: "./app/assets/scripts/app.js"
+    },
+
+    output: {
+        filename: "appMin.js",
+        path: path.resolve(__dirname, "./app/temp/scripts")
+    },
+    module: {
+        rules: [
+            {
+                loader: "babel-loader",
+                query: {
+                    presets: ["@babel/preset-env"]
+                },
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    optimization: {
+        minimizer: [new UglifyJsPlugin()],
+      }
+}
